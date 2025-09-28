0921f3ebc721a923d88cc315a559a8d7bf07e913
Keith Chow <kfc071997@hotmail.com>
2025-09-28 13:32:50 -0700

    changed URL path

--- COMP4537/labs/3/modules/utils.js
+++ COMP4537/labs/3/modules/utils.js
@@ -1,5 +1,8 @@
 module.exports = {
     getDate: function() {
         return new Date().toString();
-    }
+    },
+
+    errorMessage: "404 Not Found",
+    dateURL: "/COMP4537/labs/3/getDate/"
 }
--- COMP4537/labs/3/server.js
+++ COMP4537/labs/3/server.js
@@ -2,11 +2,17 @@ let http = require('http');
 let url = require('url');
 let strings = require('./lang/en/en.js');
 let utils = require('./modules/utils.js');
+
 http.createServer(function (req, res){
     let q = url.parse(req.url, true);
     console.log(q.query);
-    res.writeHead(200, {"Content-Type": "text/html"});
-    res.end(strings.greeting.replace('%1', q.query["name"]) + utils.getDate());
+    if(q.pathname === utils.dateURL){
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(strings.greeting.replace('%1', q.query["name"]) + utils.getDate());
+    } else {
+        res.writeHead(404, {"Content-Type": "text/html"});
+        res.end(utils.errorMessage);
+    }
 }).listen(8080);
 
-// http://localhost:8080/?name=Keith
+// http://localhost:8080/COMP4537/labs/3/getDate/?name=Keith
